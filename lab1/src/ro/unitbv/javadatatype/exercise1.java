a92618fd3e6804275497f36811a0588e2a7c5699
alexmoisa3 <alexmoisa3@gmail.com>
2020-03-04 18:54:27 +0200

    Exercitiul 1

--- lab1/src/ro/unitbv/javadatatype/exercise1.java
+++ lab1/src/ro/unitbv/javadatatype/exercise1.java
@@ -1,10 +1,36 @@
 package ro.unitbv.javadatatype;
 
+import java.util.Scanner;
+
 public class exercise1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		System.out.println("hello world");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Introdu nr 1:"); //afiseaza mesajul in consola
+		
+		int nr1=scanner.nextInt();
+		
+		System.out.println("numarul citit este:"+nr1); // afisam output-ul
+		
+		int nr2=scanner.nextInt();
+		
+		System.out.println("numarul citit este:"+nr2);
+		
+		int s=nr1+nr2;
+		
+		System.out.printf("Suma a doua numere:"+s);
+		
+        System.out.printf("Diferenta dintre 2 numere:%d%n", nr1 - nr2);
+        System.out.printf("Produs:%d%n", nr1 * nr2);
+        System.out.printf("Media:%.2f%n", (double) (nr1 + nr2) / 2);
+        System.out.printf("Distanta:%d%n", Math.abs(nr1 - nr2));
+        System.out.printf("Maxim:%d%n", Math.max(nr1, nr2));
+        System.out.printf("Minim:%d%n", Math.min(nr1, nr2));
+        	
 	}
 
 }
